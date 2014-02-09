0a963546af478fc80a3fba837db5115a3a5ea378
bgr-ubuntu-vm <bgr.gyk@gmail.com>
2014-02-09 21:25:34 +0100

    Find matches in row

--- solver.py
+++ solver.py
@@ -38,13 +38,27 @@ def print_grid(grid):
         print ''.join([str(i + 1) + ' '] + s)
 
 
-grid = get_random_grid()
-print_grid(grid)
-
-
 def column(grid, col_num):
     return [row[col_num] for row in grid]
 
 
 def transpose(grid):
     return [column(grid, i) for i in range(len(grid))]
+
+
+grid = get_random_grid()
+print_grid(grid)
+
+
+from collections import namedtuple
+Match = namedtuple('Match', 'index, type, length')
+
+
+def find_matches_in_row(row):
+    from itertools import groupby
+    groups = [Match(0, k, len(list(g))) for k, g in groupby(row)]
+    # 0 above is placeholder for index, update it to real value
+    for i, cur in list(enumerate(groups))[1:]:
+        prev = groups[i - 1]
+        groups[i] = cur._replace(index=prev.index + prev.length)
+    return [g for g in groups if g.length >= 3]
